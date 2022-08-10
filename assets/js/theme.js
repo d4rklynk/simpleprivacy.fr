b45bcf315880cdab5516a9e1e196055b810eab80
d4rklynk <d4rklynk@users.noreply.github.com>
2022-08-10 10:48:49 +0000

    deploy: 6d1e05da87cd5cb6da13ec17a2e8de18cebcccb2

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,1 +1,1 @@
-function loadPreferredTheme(){localStorage.getItem("pref-theme")==="light"&&document.body.classList.remove("dark")}loadPreferredTheme()
+function loadPreferredTheme(){const e=localStorage.getItem("pref-theme"),t=document.body.classList.contains("dark");e==="light"&&t?document.body.classList.remove("dark"):e==="dark"&&!t&&document.body.classList.add("dark")}loadPreferredTheme()
