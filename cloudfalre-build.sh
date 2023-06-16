6bba1cb46bb5a4e3a79bbe7768e2628bf7df5871
D4rkLynk <74207682+d4rklynk@users.noreply.github.com>
2023-06-16 15:13:26 +0200

    Shellchecked

--- cloudfalre-build.sh
+++ cloudfalre-build.sh
@@ -2,11 +2,11 @@
 
 curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
 
-tar xvf *.tar.gz
+tar xvf ./*.tar.gz
 chmod u+x ./hugo
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   ./hugo
 else
-  ./hugo -b $CF_PAGES_URL
+  ./hugo -b "$CF_PAGES_URL"
 fi
